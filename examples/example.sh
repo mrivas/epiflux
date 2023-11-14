756abdb3f6db96f932b24fd3b806f76613eb26cb
Bárbara Guzmán D <85778277+Barbara-GD@users.noreply.github.com>
2023-11-14 01:28:44 -0300

    Update example.sh

--- examples/example.sh
+++ examples/example.sh
@@ -1,2 +1,2 @@
 module load python/3.8.3
-python3 pheflux.py -i /home/bguzman/jupyter/UI/YMC/data/InputData_yeast_media_mod.csv -o ../results/predictions_feb22_gly_tre_o2
+python3 Epiflux.py -i /YMC/data/media/InputData_yeast_media_mod.csv -o ../results/predictions_s_cerevisiae
