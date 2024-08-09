0b12b76f130795cc72bf261fddeed4f2128fa284
Bárbara Guzmán D. <85778277+Barbara-GD@users.noreply.github.com>
2024-08-09 15:46:01 -0400

    Add files via upload

---
+++ algorithm/terminalEpiflux.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+from epiflux import *
+
+# Description:
+# ------------
+parser = argparse.ArgumentParser(description='Welcome to Epiflux.')
+
+# Arguments:
+# ----------
+parser.add_argument("-i","--input_file", help="Name of the csv file with required information.", type=str)
+parser.add_argument("-o","--output_directory", help="Name of the directory where output files will be stored.", type=str)
+
+args = parser.parse_args()
+
+print("Input file: ", args.input_file)
+
+# Create an output directory:
+# ---------------------------
+# output directory:
+resultsDir = args.output_directory
+if os.path.exists(resultsDir) == False:
+	path = os.path.join(resultsDir)
+	os.mkdir(path)
+
+# Run Epiflux:
+# ------------
+inputFile = args.input_file
+getFluxes(inputFile,resultsDir) #(-i, -o)
+
+
+print("Output directory:", resultsDir)
